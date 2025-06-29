dfde98ac4d2ebe34dc43688b06ed649cbd5fac75
sebas <sebasanguloc@gmail.com>
2025-06-28 19:18:11 -0500

    Project Creation

---
+++ src/main/java/org/api/burger_loyalty_api/BurgerLoyaltyApiApplication.java
@@ -0,0 +1,13 @@
+package org.api.burger_loyalty_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerLoyaltyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurgerLoyaltyApiApplication.class, args);
+    }
+
+}
