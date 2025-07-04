c98588b9337172798637ad25227e9d17d3ed6042
sebas <sebasanguloc@gmail.com>
2025-07-04 10:37:01 -0500

    preAuthorize added

--- src/main/java/org/api/burger_loyalty_api/controller/admin/AdminController.java
+++ src/main/java/org/api/burger_loyalty_api/controller/admin/AdminController.java
@@ -1,32 +1,38 @@
 package org.api.burger_loyalty_api.controller.admin;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clients")
 public class AdminController {
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<?> getAllClients(){
         return null;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{mobileNumber}")
     public ResponseEntity<?> getClient(@PathVariable String mobileNumber){
         return null;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/active/{mobileNumber}")
     public ResponseEntity<?> activeButton(@PathVariable String mobileNumber){
         return null;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/{mobileNumber}")
     public ResponseEntity<?> deleteClient(@PathVariable String mobileNumber){
         return null;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{mobileNumber}")
     public ResponseEntity<?> updateClient(@PathVariable String mobileNumber){
         return null;
--- src/main/java/org/api/burger_loyalty_api/controller/client/ClientController.java
+++ src/main/java/org/api/burger_loyalty_api/controller/client/ClientController.java
@@ -1,17 +1,20 @@
 package org.api.burger_loyalty_api.controller.client;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/client")
 public class ClientController {
 
+    @PreAuthorize("hasRole('CLIENT')")
     @GetMapping("/{mobileNumber}")
     public ResponseEntity<?> getClient(@PathVariable String mobileNumber){
         return null;
     }
 
+    @PreAuthorize("hasRole('CLIENT')")
     @PatchMapping("/claim/{mobileNumber}")
     public ResponseEntity<?> claimStamp(){
         return null;
